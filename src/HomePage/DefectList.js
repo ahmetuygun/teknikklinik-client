2bce312335b40c0cfa66e85a8d9d0c5cd4ea1d0c
ahmet uygun <ygnhmt@gmail.com>
2019-03-19 23:27:33 +0300

    defect list

--- src/HomePage/DefectList.js
+++ src/HomePage/DefectList.js
@@ -1,10 +1,11 @@
 
 export   const defects = [
-    { id: '1', label: 'Ekran kırık/çatlak'},
-    { id: '2', label: 'Arka kamera çalışmıyor'},
-    { id: '3', label: 'Karşıya ses gitmiyor'},
-    { id: '4', label: 'Ön kamera çalışmıyor'},
-    { id: '5', label: 'Şarj olmuyor'},
-    { id: '6', label: 'Batarya hemen bitiyor'},
-    { id: '7', label: 'Telefon birden kapanıyor'}
+    { id: '1', label: 'Ekran Değişimi'},
+    { id: '2', label: 'Batarya Değişimi'},
+    { id: '3', label: 'Arızayı Tanımlayamıyorum'}
 ];
+
+
+Ekran Değişimi
+Batarya Değişimi
+Arızayı Tanımlayamıyorum
